5479bdedf2e4b44928d08148aea07ec20f6438d8
Sammy Robens-Paradise <srobensparadise@gmail.com>
2022-04-04 12:55:00 -0400

    added test file

--- src/gradient.m
+++ src/gradient.m
@@ -1,5 +1,5 @@
 close all
-filepath="../test-data/landscape-test-2.jpeg";
+filepath="../test-data/living-room.jpeg";
 % flag to determine if intermediate plots should be output
 interm_plots=1;
 % read i the image as an matrix of RGB values
